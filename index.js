0a965797192449c0839b16d199de3207f8dac08c
itarun1 <95850621+itarun1@users.noreply.github.com>
2023-04-18 09:29:19 +0530

    commit changes

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const moment = require('moment');
+const cors = require('cors');
 const app = express();
+const port = 3000;
 
-app.get('/api/timestamp/:start/:end', (req, res) => {
-  const [start, end] = req.params.map((param) =>
-    Date.parse(param.replace(/:/g, ' '))
-  );
-  if (isNaN(start) || isNaN(end)) {
-    return res.status(400).send('Invalid date format');
-  }
-  const diffInSeconds = (end - start) / 1000;
-  res.send({ difference: diffInSeconds });
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.post('/api/timestamp-difference', (req, res) => {
+    const { timestamp1, timestamp2 } = req.body;
+    const moment1 = moment(timestamp1, 'DD:MM:YYYY HH:mm:ss', true);
+    const moment2 = moment(timestamp2, 'DD:MM:YYYY HH:mm:ss', true);
+    if (!moment1.isValid() || !moment2.isValid()) {
+      return res.status(400).json({ error: 'Invalid timestamp format' });
+    }
+    const duration = moment.duration(moment2.diff(moment1));
+    const difference = `${duration.days() +" days"} ${duration.hours()+" hours"} ${duration.minutes()+" minutes"} ${duration.seconds()}`;
+    res.json({ result: difference });
+  });
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
